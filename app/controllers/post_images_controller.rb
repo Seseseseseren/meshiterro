132ac99451319b1b8ecd0d14603fdf10336aebc1
EC2 Default User <ec2-user@ip-172-31-86-207.ec2.internal>
2021-11-11 13:17:47 +0000

    [finish]1-6

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,8 +7,12 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id 
     # ↑current_user.idについてはdivise使用時のみ利用可能
-    @post_image.save
+  if @post_image.save
     redirect_to post_images_path
+  else
+    render :new
+  end
+  
   end
   
   def index
